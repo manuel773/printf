ed0767a6b3f4081592f6f1f14ec81faaf33a27b0
root <emma.smart@gg>
2022-07-10 17:56:51 -0700

    initial commit

--- printf/main.h
+++ printf/main.h
@@ -1,27 +1,36 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef _MAIN_
+#define _MAIN_
+
+
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
+/**
+ * struct op - flag / function object
+ * @c: flag
+ * @f: function
+ */
+
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list);
+} op_t;
+
 int _printf(const char *format, ...);
-char *convert_to(char representation[], unsigned int num, int base);
-int function_manager(char c, va_list arg);
-int _switch(char c, va_list arg);
-int print_character(va_list arg);
-int print_sign(va_list arg, int base);
-int print_unsign(va_list arg, int base);
-int print_string(va_list arg);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_rev(va_list arg);
-int print_base16_upper_lower(va_list arg, char *representation);
-int loop_format(va_list arg, const char *string);
-int call_function_manager(char aux, va_list arg);
-int check_percent(int *flag, char aux);
+int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_numlist);
+int print_b(va_list binary_list);
+int print_u(va_list ar_list);
+int print_o(va_list ar_list);
+int print_x(va_list ar_list);
+int print_X(va_list ar_list);
 
 #endif
