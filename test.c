0461b3c40bfbf7cbc9b51a4eebe6e60187727f7c
Negasi Abebe <negasiabebe@gmail.com>
2022-07-12 03:15:13 -0700

    project 1

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <limits.h>
+
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+int main(void)
+{
+int n = printf("Hello me %c", 'H');
+printf("\nn = %d \n", n);
+return (0);
+}
