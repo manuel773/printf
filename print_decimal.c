ad1a984b9380aec711cd86d3d8f161024d59c1ce
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-07-13 13:07:56 +0200

    Update print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -8,16 +8,17 @@
 int print_decimal(int value)
 {
 	int count = 0;
+
 	if (value < 0);
 	{
 		count += _putchar('-');
 		value *= -1;
 	}
-	if (value/10)
+	if (value / 10)
 	{
 		int count = 0;
 
-		count += print_decimal(value/10);	
+		count += print_decimal(value / 10);
 		count += _putchar((value % 10) + '0');
 	}
 	return (count);
