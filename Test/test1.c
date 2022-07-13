a8027cfebdb38ec73d115125d18a539c7cf65b62
Negasi Abebe <negasiabebe@gmail.com>
2022-07-13 01:26:57 -0700

    task 0

---
+++ Test/test1.c
@@ -0,0 +1,47 @@
+#include <limits.h>                                                                                                                                                                   
+
+                                                                                                                                                                                      
+
+#include <stdio.h>                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+#include "main.h"                                                                                                                                                                     
+
+                                                                                                                                                                                      
+
+                                                                                                                                                                                      
+
+                                                                                                                                                                                      
+
+/**                                                                                                                                                                                   
+ *
+ *  *                                                                                                                                                                                    
+ *
+ *   *  * main - Entry point                                                                                                                                                              
+ *
+ *    *                                                                                                                                                                                    
+ *
+ *     *   *                                                                                                                                                                                
+ *
+ *      *                                                                                                                                                                                    
+ *
+ *       *    * Return: Always 0                                                                                                                                                              
+ *
+ *        *                                                                                                                                                                                    
+ *
+ *         *     */                                                                                                                                                                             
+
+                                                                                                                                                                                      
+
+int main(void)                                                                                                                                                                        
+
+{                                                                                                                                                                                     
+
+	int n = printf("Hello me %c", 'H');                                                                                                                                                   
+
+	printf("\nn = %d \n", n);                                                                                                                                                             
+
+	return (0);                                                                                                                                                                           
+
+}          a
